b57da8ce5e6e1d3059b7f25de34ec5bfe0d639bd
dennis <dennis.cpp@gmail.com>
2013-04-17 00:15:17 +0800

    add more header files

--- gen_sys_tags.sh
+++ gen_sys_tags.sh
@@ -8,11 +8,13 @@
 system_file=`mktemp	tmp.XXXXXXXXXX`
 out_file=system
 
-find /usr/include -maxdepath 1 -name "*.h" >$system_file 2>/dev/null
-find /usr/include/linux/ -name "*.h" >>$system_file 2>/dev/null
-find /usr/include/bits/  -name "*.h" >>$system_file 2>/dev/null
-find /usr/include/asm/   -name "*.h" >>$system_file 2>/dev/null
-find /usr/include/sys/   -name "*.h" >>$system_file 2>/dev/null
+find /usr/include -maxdepath 1 -name "*.h" >$system_file  2>/dev/null
+find /usr/include/linux/       -name "*.h" >>$system_file 2>/dev/null
+find /usr/include/bits/        -name "*.h" >>$system_file 2>/dev/null
+find /usr/include/asm/         -name "*.h" >>$system_file 2>/dev/null
+find /usr/include/sys/         -name "*.h" >>$system_file 2>/dev/null
+find /usr/include/arpa/        -name "*.h" >>$system_file 2>/dev/null
+find /usr/include/netinet/     -name "*.h" >>$system_file 2>/dev/null
 
 ctags -R -I --c++-kinds=+p --fields=+iaS --extra=+q -L $system_file -f $out_file
 
